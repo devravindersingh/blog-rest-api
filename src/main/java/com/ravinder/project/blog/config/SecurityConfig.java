e03f7e869803dd6c6f55ff57d65a51e72515f9f1
Ravinder Singh <ravindersngh402@outlook.com>
2023-04-15 20:29:07 +0530

    #2 implemented

--- src/main/java/com/ravinder/project/blog/config/SecurityConfig.java
+++ src/main/java/com/ravinder/project/blog/config/SecurityConfig.java
@@ -2,21 +2,53 @@ package com.ravinder.project.blog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeHttpRequests(
-                        authorizationManagerRequestMatcherRegistry ->
-                                authorizationManagerRequestMatcherRegistry.anyRequest().authenticated())
+                .authorizeHttpRequests(authorizeManager -> authorizeManager
+                                .requestMatchers(HttpMethod.GET, "/api/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
                 .httpBasic(Customizer.withDefaults());
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails userRavinder = User.builder()
+                .username("ravinder")
+                .password(passwordEncoder().encode("ravinder"))
+                .roles("USER")
+                .build();
+        UserDetails userAdmin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("root"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userRavinder, userAdmin);
     }
 }
--- src/main/java/com/ravinder/project/blog/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ravinder/project/blog/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -51,17 +52,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
-    //global exceptions
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorDetails> handleGlobalException(
-            Exception exception, WebRequest webRequest){
-        ErrorDetails errorDetails = new ErrorDetails(
-                new Date(),
-                exception.getMessage(),
-                webRequest.getDescription(false));
-        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
@@ -75,4 +65,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         });
         return new ResponseEntity<>(errorsMap, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<ErrorDetails> handleAccessDeniedException(
+            AccessDeniedException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.UNAUTHORIZED);
+    }
+
+    //global exceptions
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDetails> handleGlobalException(
+            Exception exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(
+                new Date(),
+                exception.getMessage(),
+                webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+
 }
