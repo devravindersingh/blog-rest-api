5358a07ff1b87d998bd85776746263334ee8e698
Ravinder Singh <ravindersngh402@outlook.com>
2023-04-16 23:10:58 +0530

    #4 project wide imports optimized

--- src/main/java/com/ravinder/project/blog/BlogApplication.java
+++ src/main/java/com/ravinder/project/blog/BlogApplication.java
@@ -1,10 +1,8 @@
 package com.ravinder.project.blog;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
--- src/main/java/com/ravinder/project/blog/entity/Comment.java
+++ src/main/java/com/ravinder/project/blog/entity/Comment.java
@@ -1,7 +1,10 @@
 package com.ravinder.project.blog.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Setter
 @Getter
